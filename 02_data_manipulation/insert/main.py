1395012e67571522e8c186edd7a4f2a40f2928ac
r3card0 <ricardo.rdz@pm.me>
2023-06-12 19:43:24 -0600

    main modified

--- 02_data_manipulation/insert/main.py
+++ 02_data_manipulation/insert/main.py
@@ -1,21 +1,24 @@
 # libraries
 from league_insert import Leagues
+from season_insert import Seasons
+from _path import leagues_file, seasons_file
 
 # data
 
 # instances
-league_mx = Leagues('/Users/ideasleon/platzi_edu/Bases_de_Datos/PostgreSQL/proyectos/soccer/soccer_db/02_data_manipulation/data/leagues.csv',['league_name','league_lvl','country_id',])
-
+league_mx = Leagues(leagues_file,['league_name','league_lvl','country_id',])
+season_mx = Seasons(seasons_file,['season','league_id','start_date','end_date'])
 
 # operations / actions
-leagues_insert = league_mx.insert_data_by_row()
+# leagues_insert = league_mx.insert_data_by_row()
+season_insert = season_mx.insert_data_by_row()
 
 # entry point
 def run():
     print('Starting 🕐 . . .')
 
     # instance operation
-    leagues_insert
+    season_insert
 
 
     print("Insertion it's done 😎")
