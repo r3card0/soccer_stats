f7fbbbc88bf32db12e639cf90db49c21f8278cd4
r3card0 <ricardo.rdz@pm.me>
2023-06-13 12:11:34 -0600

    teams by  season instance

--- 02_data_manipulation/insert/main.py
+++ 02_data_manipulation/insert/main.py
@@ -2,7 +2,8 @@
 from league_insert import Leagues
 from season_insert import Seasons
 from team_insert import Teams
-from _path import leagues_file, seasons_file, teams_file
+from teams_by_season import TeamsBySeason
+from _path import leagues_file, seasons_file, teams_file, teams_by_season_file
 
 # data
 
@@ -10,20 +11,21 @@ from _path import leagues_file, seasons_file, teams_file
 league_mx = Leagues(leagues_file,['league_name','league_lvl','country_id',])
 season_mx = Seasons(seasons_file,['season','league_id','start_date','end_date'])
 teams_mx = Teams(teams_file,['team','team_type','country_id'])
-
+teams_season_mx = TeamsBySeason(teams_by_season_file,['season_id','team_id'])
 
 
 # operations / actions
 # leagues_insert = league_mx.insert_data_by_row()
 # season_insert = season_mx.insert_data_by_row()
-team_insert = teams_mx.insert_data_by_row()
+# team_insert = teams_mx.insert_data_by_row()
+tbs_insert = teams_season_mx.insert_data_by_row()
 
 # entry point
 def run():
     print('Starting 🕐 . . .')
 
     # instance operation
-    team_insert
+    tbs_insert
 
 
     print("Insertion it's done 😎")
