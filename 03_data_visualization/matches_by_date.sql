1e57f4b4659da9b04488199bf9e6d0c404adbb8e
r3card0 <ricardo.rdz@pm.me>
2023-06-17 15:51:37 -0600

    added jornada and fase fields

--- 03_data_visualization/matches_by_date.sql
+++ 03_data_visualization/matches_by_date.sql
@@ -6,6 +6,13 @@ to_char(match_date ,'YYYY-MM-DD') fecha
 , lt.team equipo_local
 , vt.team equipo_visitante
 , concat(lt.team, ' vs ' ,vt.team) partido
+, m.week_no jornada
+, 
+case
+	when round = 0 then 'regular'
+	when round = 1 then 'liguilla'
+	when round = 2 then 'repechaje'
+end fase
 , st.geom
 FROM matches m
 JOIN stadiums_mx st ON st.id = m.stadium_id
