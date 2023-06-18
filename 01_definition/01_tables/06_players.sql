9cbaa7caa6914472feb51463f660b80e14c28574
r3card0 <ricardo.rdz@pm.me>
2023-06-17 20:47:26 -0600

    players table created

---
+++ 01_definition/01_tables/06_players.sql
@@ -0,0 +1,12 @@
+CREATE TABLE players
+(
+    player_id SERIAL NOT NULL
+    , player_name VARCHAR(30) NOT NULL
+    , last_name VARCHAR(30) NOT NULL
+    , nick_name VARCHAR(30)
+    , birthday DATE
+    , nationality INTEGER
+    , insert_date TIMESTAMP DEFAULT NOW()
+    , CONSTRAINT player_id_pkey PRIMARY KEY (player_id)
+
+)
