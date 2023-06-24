ce34011c4fbfb35c470bd8a3c27ee0fec6d110c0
r3card0 <ricardo.rdz@pm.me>
2023-06-24 11:38:11 -0600

    example class to insert data into dbt

---
+++ 02_data_manipulation/apps/app_season/submit.py
@@ -0,0 +1,13 @@
+from moco import Moco
+
+# Instances
+submit = Moco()
+
+# entry point
+def run():
+    # submit.submit_create_table()
+    submit.submit_drop_table()
+
+
+if __name__ == "__main__":
+    run()
