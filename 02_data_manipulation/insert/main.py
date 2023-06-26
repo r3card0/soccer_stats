fcc5ea52156216687d6c5d4446631cf06d2c8995
r3card0 <ricardo.rdz@pm.me>
2023-06-25 19:41:30 -0600

    match details instance added

--- 02_data_manipulation/insert/main.py
+++ 02_data_manipulation/insert/main.py
@@ -8,7 +8,8 @@ from matches_insert import Matches
 from players_insert import Players
 from players_by_team_by_season_insert import Players_team_season
 from match_players_insert import Match_players
-from _path import leagues_file, seasons_file, teams_file, teams_by_season_file, stadiumByTeams, matches, players, players_by_ts, match_players_data
+from match_details_insert import MatchDetails
+from _path import leagues_file, seasons_file, teams_file, teams_by_season_file, stadiumByTeams, matches, players, players_by_ts, match_players_data, match_details_data
 
 # data
 
@@ -23,6 +24,7 @@ matches_mx = Matches(matches,['match_no','date','stadiumId','local_team','visito
 players_test = Players(players,['nombre','apellido'])
 pts = Players_team_season(players_by_ts,['season_id','team_id','player_id','tshirt'])
 match_players = Match_players(match_players_data,['match_id','player_id','alineacion_inicial'])
+md = MatchDetails(match_details_data,['match_id','half','minute','action_id','player_id'])
 
 # operations / actions
 # leagues_insert = league_mx.insert_data_by_row()
@@ -33,18 +35,17 @@ match_players = Match_players(match_players_data,['match_id','player_id','alinea
 # stadium_by_team_insert = stadium_by_team.insert_data_by_row()
 # matches_insert_ = matches_mx.insert_data_by_row()
 # players_insert_ = players_test.insert_data_by_row()
-# playerby_ts_insert_ = pts.insert_data_by_row()
-match_players_insert_ = match_players.insert_data_by_row()
+playerby_ts_insert_ = pts.insert_data_by_row()
+# match_players_insert_ = match_players.insert_data_by_row()
+# md_insert_ = md.insert_data_by_row()
 
 
 
 # entry point
 def run():
     print('Starting 🕐 . . .')
-    match_players_insert_
-
-    # print(stadium_by_team.read_csv_())
-    
+      
+    playerby_ts_insert_
 
 
     print("Insertion it's done 😎")
